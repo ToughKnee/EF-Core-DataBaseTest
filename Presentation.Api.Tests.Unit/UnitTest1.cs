aeb00a85ecaffe7c14cf81eb0092d45918fbc281
Carlos Antonio <Carlos.sanchezblanco@ucr.ac.cr>
2023-09-25 01:32:15 -0600

    Add project files.

---
+++ Presentation.Api.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CleanArchitectureWorkshop.Presentation.Api.Tests.Unit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
